d43f96bde15ae5daf26a6751a77a397f88865611
Atom Labs <atombaseinc@gmail.com>
2025-09-17 12:12:35 +0530

    Logging & Exceptions

--- src/main/java/com/mtd/ecom_server/controllers/ProductController.java
+++ src/main/java/com/mtd/ecom_server/controllers/ProductController.java
@@ -1,7 +1,10 @@
 package com.mtd.ecom_server.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,27 +23,33 @@ import com.mtd.ecom_server.repos.ProductRepo;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+	private static final Logger log = LoggerFactory.getLogger(ProductController.class);
+	
 	@Autowired ProductRepo productRepo;
 	
 	@GetMapping("/all")
 	public List<Product> getAllProducts() {
+		log.info("fetching products");
 		return productRepo.findAll();
 	}
 	@PostMapping("/add")
 	public Product addProduct(@RequestBody Product newproduct) {
+		log.info("Adding Product"+ newproduct);
 		return productRepo.save(newproduct);
 	}
+	
 	@DeleteMapping("/product/delete/{id}")
 	public String deleteProduct(@PathVariable String id) {
-		Product findproduct  = productRepo.findById(id).get();
-		if(findproduct !=null) {
-			productRepo.deleteById(id);
-			return "Product Deleted "+ findproduct.getName();
-		}
-		else {
+	Optional<Product> findproduct  = productRepo.findById(id);
+		if(findproduct.isEmpty()) {
+			log.error("Failed to delete product"+id);
 			return "Failed to delete product";
 		}
+		productRepo.deleteById(id);
+		log.info("Product Deleted "+id);
+		return "Product Deleted ";
 	}
+	
 	@PutMapping ("/product/edit/{id}")
 	public Product editPorduct(@PathVariable String id, @RequestBody Product newproduct) {
 		Product findproduct = productRepo.findById(id).get();
@@ -50,6 +59,7 @@ public class ProductController {
 		findproduct.setTags(newproduct.getTags());
 		findproduct.setPrice(newproduct.getPrice());
 		findproduct.setStock(newproduct.getStock());
+		log.info("Updating the product"+findproduct);
 		return productRepo.save(findproduct) ;
 	}
 	
